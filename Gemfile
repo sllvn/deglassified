7f0cbe1b5cc57fd3eabdf3fa0dfca4f9b3ca4035
Andrew Sullivan <licyeus@gmail.com>
2013-12-07 12:56:33 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.0.2'
+gem 'pg'
+gem 'puma'
+
+# front-end gems
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'foundation-rails'
+gem 'mapbox-rails'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development do
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'pry-rescue'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WithoutGlass::Application.config.secret_key_base = '6580dff4b826171376b0463bfebbb355d618a679d8fd770148f9fa3404e3b4d4d404c21a7e1095e556fcd450a740b5a3d420e99e33a80302fed7337813842a7f'
