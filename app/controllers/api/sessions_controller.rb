178948fe5cd02efde77894f9e7027705c6ed3aab
Andrew Sullivan <licyeus@gmail.com>
2014-02-19 21:09:55 -0800

    Add registration API responses

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,15 +2,15 @@ class Api::SessionsController < Devise::SessionsController
   def create
     # there seems to be a bug with warden's authenticate! where recall never is actually called
     user = warden.authenticate!(scope: resource_name, recall: "#{controller_path}#failure")
-    render json: { success: true, authentication_token: user.authentication_token }
+    render json: { status: 'success', auth: { token: user.authentication_token } }
   end
 
   def destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    render json: {}
+    render json: { status: 'success' }
   end
 
   def failure
-    render json: { success: false, errors: ["Login failed"] }
+    render json: { status: 'failure', errors: ["Login failed"] }
   end
 end
