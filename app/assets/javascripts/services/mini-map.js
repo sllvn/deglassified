e059be8dabc9dd3ed94ab8787842433534f8ae55
Andre Levi <andrelevi09@gmail.com>
2014-02-25 23:24:08 -0800

    Added popup content for minimap marker.

--- app/assets/javascripts/services/mini-map.js
+++ app/assets/javascripts/services/mini-map.js
@@ -18,30 +18,31 @@ angular.module('service.mini-map', [])
     var marker;
 
     function showBusiness(coords, business) {
-        console.log(coords); 
+        console.log(business); 
         if (coords) {
             panTo(coords);
         }
         marker = L.marker([coords.lat, coords.lng], { draggable:true }).addTo(map);
         bindMarkerPopup(marker, business);
         marker.on('dragend', handleMarkerDrag);
+        marker.openPopup();
     }
 
     function bindMarkerPopup(marker, business) {
         var content  = "<h4>" + business.name + "</h4>" +
             "<p>" + business.address + "</p>";
-        if (business.links)
+        if (business.links) {
             content += '<p>';
-        if (business.links.website)
-            content += "<a href='" + business.links.website + "' target='_blank'><i class='fi-link'></i> website</a><br>";
-        if (business.links.facebook)
-            content += "<a href='" + business.links.facebook + "' target='_blank'><i class='fi-social-facebook'></i> facebook</a><br>";
-        if (business.links.twitter)
-            content += "<a href='" + business.links.twitter + "' target='_blank'><i class='fi-social-twitter'></i> twitter</a><br>";
-        if (business.links.yelp)
-            content += "<a href='" + business.links.yelp + "' target='_blank'><i class='fi-social-yelp'></i> yelp</a><br>";
-        content += '</p>';
-
+            if (business.links.website)
+                content += "<a href='" + business.links.website + "' target='_blank'><i class='fi-link'></i> website</a><br>";
+            if (business.links.facebook)
+                content += "<a href='" + business.links.facebook + "' target='_blank'><i class='fi-social-facebook'></i> facebook</a><br>";
+            if (business.links.twitter)
+                content += "<a href='" + business.links.twitter + "' target='_blank'><i class='fi-social-twitter'></i> twitter</a><br>";
+            if (business.links.yelp)
+                content += "<a href='" + business.links.yelp + "' target='_blank'><i class='fi-social-yelp'></i> yelp</a><br>";
+            content += '</p>';
+        }
         marker.bindPopup(content);
     }
 
