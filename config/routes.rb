bbf57f28b107cc1e02e8fe446535e35311b63fce
Andrew Sullivan <licyeus@gmail.com>
2014-03-06 14:23:14 -0800

    Add business route outside of locations nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Deglassified::Application.routes.draw do
   scope :api do
     devise_for :users, controllers: { sessions: 'api/sessions', registrations: 'api/registrations' }
+    resources :businesses, only: [:index, :show, :create, :update]
     resources :locations, only: [:index, :show, :create, :update] do
       resources :businesses, only: [:index, :show, :create, :update]
     end
