6887dfe0d656abe1fb24ca07385e36deb20b5fa3
Andrew Sullivan <licyeus@gmail.com>
2014-03-08 17:02:49 -0800

    Update geocoding service to use Google Geocoding API

--- app/services/geocoder.rb
+++ app/services/geocoder.rb
@@ -1,14 +1,14 @@
 class Geocoder
   def self.geocode(city)
     begin
-      response = open("http://api.geocod.io/v1/geocode?api_key=#{ENV['GEOCODIO_API_KEY']}&q=#{URI.escape(city)}")
+      response = open("https://maps.googleapis.com/maps/api/geocode/json?key=#{ENV['GOOGLE_GEOCODING_API_KEY']}&sensor=false&address=#{URI.escape(city)}")
       geocoded = JSON.parse(response.string)
       result = geocoded['results'].first
       {
-        formatted_city: result['address_components']['city'],
-        formatted_state: result['address_components']['state'],
-        lat: result['location']['lat'].to_f,
-        lng: result['location']['lng'].to_f
+        formatted_city: result['address_components'][0]['short_name'],
+        formatted_state: result['address_components'][2]['short_name'],
+        lat: result['geometry']['location']['lat'].to_f,
+        lng: result['geometry']['location']['lng'].to_f
       }
     rescue
       nil
