cd48db770c2f726716d6894201c2eaf4eebefbf4
Andre Levi <andrelevi09@gmail.com>
2014-03-21 21:50:15 -0700

    Using resolve for location state results in a race condition bug.  Putting promise resolution back in controller.

--- app/assets/javascripts/states/location-state.js
+++ app/assets/javascripts/states/location-state.js
@@ -9,32 +9,30 @@ angular.module('state.location', [
     $stateProvider.state('location', {
         url: '/:location',
         templateUrl: '/partials/location-data.html',
-        controller: 'locationCtrl',
-        resolve: {
-            locationData: function($stateParams, locationDataService) {
-                return locationDataService.getSingle($stateParams.location);
-            }
-        }
+        controller: 'locationCtrl'
     });
 
 })
 
-.controller('locationCtrl', function($rootScope, $scope, $state, mainMapService, locationData) {
-    if (locationData === 404) {
-        $state.go('404');
-    } else if (locationData.slug) {
-        loadLocation(locationData);
-    }
+.controller('locationCtrl', function($rootScope, $scope, $state, $stateParams, mainMapService, locationDataService) {
+    locationDataService.getSingle($stateParams.location)
+        .then(function(locationData) {
+            if (locationData === 404) {
+                $state.go('404');
+            } else if (locationData.slug) {
+                loadLocation(locationData);
+            }
+        });
 
     function loadLocation(locationData) {
         $rootScope.pageTitle = locationData.city;
         $rootScope.currentLocation = locationData;
         $scope.currentCity = locationData.city;
         $scope.businesses = locationData.businesses;
-        loadLocationInMapbox();
+        loadLocationInMapbox(locationData);
     }
 
-    function loadLocationInMapbox() {
+    function loadLocationInMapbox(locationData) {
         mainMapService.loadLocation(locationData);
         $scope.mapboxMarkersLoaded = true;
         $scope.$broadcast('mapboxMarkersLoaded');
